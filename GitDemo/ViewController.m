b2faab91fd51399f7c9c7ac376a08d4112c3e2fe
zhangleoi <zhanglei@zhangleidemac-mini.local>
2015-11-16 13:29:05 +0800

    first change

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic) int sum;
 @end
 
 @implementation ViewController
@@ -17,6 +17,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    int a = 5;
+    int b = 10;
+    
+    self.sum = a + b;
+    
+    NSLog(@"The result is: %d", self.sum);
 }
 
 - (void)didReceiveMemoryWarning {
