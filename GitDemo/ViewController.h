7e0b7abaa5435391818e2c8f14b9c7d0232c2b2d
zhangleoi <zhanglei@zhangleidemac-mini.local>
2015-11-16 13:16:22 +0800

    Initial Commit

---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by 张磊 on 15/11/16.
+//  Copyright © 2015年 ZhengMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitDemo
+//
+//  Created by 张磊 on 15/11/16.
+//  Copyright © 2015年 ZhengMu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
